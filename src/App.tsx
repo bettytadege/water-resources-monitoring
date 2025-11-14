da61017db5a59dae66f6b3f3c882fc78e50844d6
bettytadege <bettytadege13@gmail.com>
2025-11-14 10:46:02 +0300

    feat:add feature route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Features from "./components/feature/Features"
+
 
 
 function App() {
   return (
-    <div>App</div>
+    <div className=" w-full h-full bg-linear-to-r from-slate-50 to-cyan-50 p-6">
+      <Routes>
+        <Route path="/" element={ <Home/>}>
+        <Route path="/" element={ <Features/> }/>
+        </Route>
+      </Routes>
+     
+    </div>
   )
 }
 
