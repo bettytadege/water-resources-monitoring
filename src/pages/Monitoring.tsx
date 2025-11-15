01e52465bca7b307bc346364b66060556d6e6d96
bettytadege <bettytadege13@gmail.com>
2025-11-15 15:04:59 +0300

    feat:add header in this page

--- src/pages/Monitoring.tsx
+++ src/pages/Monitoring.tsx
@@ -1,7 +1,12 @@
+import Header from "@/components/monitoring/Header"
 
 function Monitoring() {
   return (
-    <div>Monitoring</div>
+    <>
+    <div className="bg-white m-6 ">
+      <Header/>
+    </div>
+    </>
   )
 }
 
