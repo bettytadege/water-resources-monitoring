5fc1bd5fa37ae9a7b936805dcd16634b4bb7710f
bettytadege <bettytadege13@gmail.com>
2025-11-15 12:56:30 +0300

    chore:remove unnecessary gap

--- src/components/system-overview/SystemOverview.tsx
+++ src/components/system-overview/SystemOverview.tsx
@@ -8,7 +8,7 @@ function SystemOverview() {
         <>
             <div className="grid  gap-5 lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
                 {systemOverviewData.map((data) => (
-                    <SystemCard borderColor={data.borderColor} value={data.value} title={data.title} subtitle={data?.subtitle} key={data.id} icon={data.icon} statusColor={data.statusColor} />
+                    <SystemCard  borderColor={data.borderColor} value={data.value} title={data.title} subtitle={data?.subtitle} key={data.id} icon={data.icon} statusColor={data.statusColor} />
                 ))}
 
             </div>
