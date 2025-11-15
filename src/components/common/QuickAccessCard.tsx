40da7dae9ecf2439822cff0b474fa7a58195dd34
bettytadege <bettytadege13@gmail.com>
2025-11-15 12:28:12 +0300

    feat:implement routing to naviagte between pages

--- src/components/common/QuickAccessCard.tsx
+++ src/components/common/QuickAccessCard.tsx
@@ -1,16 +1,19 @@
 import { ReactNode } from "react"
+import { useNavigate } from "react-router-dom"
 
 type QuickAccessCard = {
   icon: ReactNode,
   name: string,
+  route: string,
 }
 
-function QuickAccessCard({icon:Icon,name}:QuickAccessCard) {
+function QuickAccessCard({ icon: Icon, name, route }: QuickAccessCard) {
+  const navigate = useNavigate()
   return (
-    <div className='border border-gray-300 flex flex-col items-center gap-2 justify-center bg-inherit h-20 font-medium w-48 rounded-md hover:bg-[#8B5CF6] group '>
-            <Icon className="text-sky-blue" size={18} />
-            <p className="text-deep-teal text-xs group-hover:text-white">{name}</p>
-       
+    <div onClick={() => navigate(route)} className='border border-gray-300 flex flex-col items-center gap-2 justify-center bg-inherit h-20 font-medium w-full rounded-md hover:bg-[#8B5CF6] group '>
+      <Icon className="text-sky-blue" size={18} />
+      <p className="text-deep-teal text-xs group-hover:text-white">{name}</p>
+
     </div>
   )
 }
