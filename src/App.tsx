3e5ed5499789ae725297c1d3b2c3de82f192a8da
bettytadege <bettytadege13@gmail.com>
2025-11-15 12:36:38 +0300

    feat: add routing for monitoring, geospatial, AI forecasting, and early warning

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Features from "./components/feature/Features"
+import DataInegration from "./components/data-integration/DataInegration"
+import Monitoring from "./pages/Monitoring"
+import GeoSpatial from "./pages/GeoSpatial"
+import AiForecasting from "./pages/AiForecasting"
+import EarlyWarning from "./pages/EarlyWarning"
+import SystemTesting from "./components/system-testing/SystemTesting"
 
 
 
@@ -10,7 +16,13 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home/>}>
         <Route path="/" element={ <Features/> }/>
+        <Route path="/data-integration" element={ <DataInegration/> }/>
+        <Route path="/system-testing" element={ <SystemTesting/> }/>
         </Route>
+        <Route path="/monitoring" element={ <Monitoring/> }/>
+        <Route path="/geospatial" element={ <GeoSpatial/> }/>
+        <Route path="/ai-forecasting" element={ <AiForecasting/> }/>
+        <Route path="/early-warning" element={ <EarlyWarning/> }/>
       </Routes>
      
     </div>
