2d6342c12a30f529795322a16c65a309210de892
bettytadege <bettytadege13@gmail.com>
2025-11-15 12:25:49 +0300

    feat(ui):add resuable data flow card

---
+++ src/components/common/DataFlowCard.tsx
@@ -0,0 +1,26 @@
+
+import { IoCheckmark } from "react-icons/io5";
+
+type DataFlowCardProps = {
+  name: string;
+  status: string;
+};
+
+function DataFlowCard({ name, status }: DataFlowCardProps) {
+  return (
+    <>
+
+      <div className="flex justify-between space-y-">
+        <ul>
+          <li className=" text-slate-purple">{name}</li>
+        </ul>
+        <button className={`${status === 'Active' || status === 'Passed'
+          ? 'bg-[#D0FAE4] ' : 'bg-[#CEFAFE]'} text-[#006044] py-0.5 px-2 text-xs rounded-md flex items-center `}>{status === 'Passed' ?
+            <IoCheckmark /> : ''} {status}</button>
+      </div>
+
+    </>
+  )
+}
+
+export default DataFlowCard
