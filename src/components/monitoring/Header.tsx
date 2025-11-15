46650cfb9303c49f4888b69a1c5b9a05935cc89d
bettytadege <bettytadege13@gmail.com>
2025-11-15 15:04:31 +0300

    feat:create header for monitoring

---
+++ src/components/monitoring/Header.tsx
@@ -0,0 +1,27 @@
+import Fire from "@/assets/svg/Fire"
+import { Button } from "../ui/button"
+import { BsActivity } from "react-icons/bs"
+import { Satellite } from "lucide-react"
+
+
+function Header() {
+  return (
+    <div className="flex justify-between">
+        <div className="flex items-center gap-4">
+            <div className="bg-[#E7EDEF] rounded-md w-12 h-12 flex items-center justify-center">
+                <Fire />
+            </div>
+            <div className="text-slate-purple">
+                <p className="text-3xl font-bold">AmbaLay Water Monitoring</p>
+                <span className="text-sm">Ethiopian Space Science & Geospatial Institute</span>
+            </div>
+        </div>
+        <div className="flex gap-5">
+            <Button className="bg-inherit border hover:bg-inherit py-0 border-deep-teal text-deep-teal"><BsActivity />Live Data</Button>
+            <Button  className="bg-inherit border hover:bg-deep-teal hover:text-white  text-slate-purple"><Satellite/>Satellite View</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
