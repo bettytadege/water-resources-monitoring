e3025b056b007794167bf7a033cadd5061de3aa9
bettytadege <bettytadege13@gmail.com>
2025-11-16 14:17:19 +0300

    feat:implememt header,alert card and outlet for routing

--- src/pages/Monitoring.tsx
+++ src/pages/Monitoring.tsx
@@ -1,10 +1,43 @@
+import ActionBar from "@/components/common/ActionBar"
 import Header from "@/components/monitoring/Header"
+import WaterLevelAlertCard from "@/components/monitoring/WaterLevelAlertCard"
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
+const actionBarElements = [
+  { id: "1", name: "Overview", route: "/monitoring" },
+  { id: "2", name: "Satellite Data", route: "/monitoring/overview" },
+  { id: "3", name: "Sensor Network", route: "/monitoring/overview" },
+  { id: "4", name: "Ai Forecasting", route: "/monitoring/overview" },
+];
 function Monitoring() {
+   const [activeId, setActiveId] = useState("1");
   return (
     <>
-    <div className="bg-white m-6 ">
+    <div className="bg-white">
+      <div className="mx-6 my-5 top-0 sticky">
       <Header/>
+      </div>
+     <hr className="w-full border-neutral-300 " />
+     <div className="m-6 flex-col flex gap-5">
+      <WaterLevelAlertCard/>
+      <div className="bg-[#F7F9FB] w-full rounded-lg  h-10 py-1 flex flex-row justify-around  ">
+        {actionBarElements.map((item) => (
+          <ActionBar
+            key={item.id}
+            name={item.name}
+            route={item.route}
+            id={item.id}
+            isActive={activeId === item.id}
+            onActivate={() => setActiveId(item.id)}
+          />
+        ))}
+      </div>
+      
+     </div>
+     <div className="m-6">
+        <Outlet/>
+      </div>
     </div>
     </>
   )
