f17207d8e1f655681cb10475d1ffd5d804bf6337
bettytadege <bettytadege13@gmail.com>
2025-11-16 14:15:16 +0300

    feat:add water level aler card

---
+++ src/components/monitoring/WaterLevelAlertCard.tsx
@@ -0,0 +1,20 @@
+
+import { PiWarningBold } from "react-icons/pi"
+
+function WaterLevelAlertCard() {
+    return (
+        <>
+            <div className="border border-[#e7c3cc] bg-[#FCF3F5] rounded-md py-4 px-3 ">
+                <div className="flex gap-4 items-start">
+                    <PiWarningBold className="text-[#BE123c] w-4" fontWeight={600} />
+                    <div>
+                        <p className="text-sm text-[#BE123c] font-semibold tracking-tight">Water Level Alert</p>
+                        <p className="text-sm text-slate-purple">Koka Dam water level is below normal threshold. Monitoring team has been notified.</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default WaterLevelAlertCard
