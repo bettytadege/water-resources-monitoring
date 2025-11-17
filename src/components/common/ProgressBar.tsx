beded21c195ea4cf874cc6f759db707a11dc007e
bettytadege <bettytadege13@gmail.com>
2025-11-17 05:37:37 +0300

    style:add indicator color

--- src/components/common/ProgressBar.tsx
+++ src/components/common/ProgressBar.tsx
@@ -1,9 +1,13 @@
 import { Progress } from "../ui/progress"
 
-
-function ProgressBar({width}:{width:number}) {
+function ProgressBar({ width }: { width: number }) {
   return (
-    <Progress value={width}   className={"bg-gray-300 w-full"}/>
+    <Progress
+      value={width}
+      className="w-full bg-gray-300"
+      color="#164E63"
+      
+    />
   )
 }
 
