70fa61836fe1e06e4320c5eac44b7ac866912f46
bettytadege <bettytadege13@gmail.com>
2025-11-17 05:41:28 +0300

    feat:add water level trend chart

---
+++ src/components/monitoring/overview/WaterLevelTrend.tsx
@@ -0,0 +1,15 @@
+
+function WaterLevelTrend() {
+    return (
+        <div className="bg-ice-blue rounded-xl border p-5 shadow">
+            <div className="">
+                <p className="text-deep-teal font-semibold text-xl">Water Level Trends
+                </p>
+                <span className="text-slate-purple text-sm">Monthly water levels and rainfall correlation</span>
+            </div>
+            <div className=""></div>
+        </div>
+    )
+}
+
+export default WaterLevelTrend
