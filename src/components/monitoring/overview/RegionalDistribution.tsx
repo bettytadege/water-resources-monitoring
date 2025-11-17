51b1cd99d4faf005c8e19d663e6d389ed6df5705
bettytadege <bettytadege13@gmail.com>
2025-11-17 05:40:19 +0300

    feat:add regional distribution chart

---
+++ src/components/monitoring/overview/RegionalDistribution.tsx
@@ -0,0 +1,15 @@
+
+
+function RegionalDistribution() {
+  return (
+    <div className="bg-ice-blue rounded-xl border p-5 shadow">
+      <div className="">
+                <p className="text-deep-teal font-semibold text-xl">Regional Distribution
+                </p>
+                <span className="text-slate-purple text-sm">Water monitoring coverage by region</span>
+            </div>
+    </div>
+  )
+}
+
+export default RegionalDistribution
