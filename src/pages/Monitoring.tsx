ee5474ac4049ec6f1c2aef6d98512583e9211adf
bettytadege <bettytadege13@gmail.com>
2025-11-17 05:34:18 +0300

    style:make header sticky and apply padding

--- src/pages/Monitoring.tsx
+++ src/pages/Monitoring.tsx
@@ -14,31 +14,31 @@ function Monitoring() {
    const [activeId, setActiveId] = useState("1");
   return (
     <>
-    <div className="bg-white">
-      <div className="mx-6 my-5 top-0 sticky">
-      <Header/>
+      <div className="bg-white">
+      <div className="sticky top-0  p-5">
+      <Header />
+  </div>
+        <hr className="w-full border-neutral-300 " />
+        <div className="m-6 flex-col flex gap-5">
+          <WaterLevelAlertCard />
+          <div className="bg-[#F7F9FB] w-full rounded-lg  h-10 py-1 flex flex-row justify-around  ">
+            {actionBarElements.map((item) => (
+              <ActionBar
+                key={item.id}
+                name={item.name}
+                route={item.route}
+                id={item.id}
+                isActive={activeId === item.id}
+                onActivate={() => setActiveId(item.id)}
+              />
+            ))}
+          </div>
+
+        </div>
+        <div className="m-6">
+          <Outlet />
+        </div>
       </div>
-     <hr className="w-full border-neutral-300 " />
-     <div className="m-6 flex-col flex gap-5">
-      <WaterLevelAlertCard/>
-      <div className="bg-[#F7F9FB] w-full rounded-lg  h-10 py-1 flex flex-row justify-around  ">
-        {actionBarElements.map((item) => (
-          <ActionBar
-            key={item.id}
-            name={item.name}
-            route={item.route}
-            id={item.id}
-            isActive={activeId === item.id}
-            onActivate={() => setActiveId(item.id)}
-          />
-        ))}
-      </div>
-      
-     </div>
-     <div className="m-6">
-        <Outlet/>
-      </div>
-    </div>
     </>
   )
 }
