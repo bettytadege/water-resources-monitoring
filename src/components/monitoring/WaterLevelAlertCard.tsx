ceec88ed0fd3dbfb3ac0c0d053ce12ed62d1f49a
bettytadege <bettytadege13@gmail.com>
2025-11-18 09:56:29 +0300

    style:update textcolor with custom theme color

--- src/components/monitoring/WaterLevelAlertCard.tsx
+++ src/components/monitoring/WaterLevelAlertCard.tsx
@@ -6,9 +6,9 @@ function WaterLevelAlertCard() {
         <>
             <div className="border border-[#e7c3cc] bg-[#FCF3F5] rounded-md p-3 ">
                 <div className="flex gap-4 items-start">
-                    <PiWarningBold className="text-[#BE123c] w-4" fontWeight={600} />
+                    <PiWarningBold className="text-dark-red w-4" fontWeight={600} />
                     <div>
-                        <p className="text-sm text-[#BE123c] font-semibold tracking-tight">Water Level Alert</p>
+                        <p className="text-sm text-dark-red font-semibold tracking-tight">Water Level Alert</p>
                         <p className="text-sm text-slate-purple">Koka Dam water level is below normal threshold. Monitoring team has been notified.</p>
                     </div>
                 </div>
