9db9f9107a74d276d7f1af74930bda0952e3fac2
bettytadege <bettytadege13@gmail.com>
2025-11-22 10:27:34 +0300

    feat:add action bar elments and header text

--- src/pages/GeoSpatial.tsx
+++ src/pages/GeoSpatial.tsx
@@ -1,8 +1,44 @@
+import ActionBar from "@/components/common/ActionBar"
+import { useState } from "react";
+import { Outlet } from "react-router-dom";
 
-
+const actionBarElements = [
+  { id: "1", name: "Watersheds", route: "/geospatial" },
+  { id: "2", name: " Routing", route: "/monitoring/satellite-data" },
+  { id: "3", name: " Geocoding", route: "/monitoring/sensor-network" },
+  { id: "4", name: "Aquifers", route: "/monitoring/ai-forecasting" },
+  { id: "5", name: "Intrgration", route: "/monitoring/ai-forecasting" },
+];
 function GeoSpatial() {
+    const [activeId, setActiveId] = useState("1");
   return (
-    <div>GeoSpatial</div>
+    <>
+    <div className="mx-24 my-8 space-y-4">
+      <div className="text-slate-purple text-center">
+      <h1 className="text-3xl font-bold  ">
+          Geospatial API Infrastructure
+        </h1>
+        <p>
+          AmbaLay Maps APIs for hydrological mapping, routing, and system integration
+        </p>
+        </div>
+       <div className="bg-[#F7F9FB] w-full rounded-lg  h-10 py-1 flex flex-row justify-around  ">
+            {actionBarElements.map((item) => (
+              <ActionBar
+                key={item.id}
+                name={item.name}
+                route={item.route}
+                id={item.id}
+                isActive={activeId === item.id}
+                onActivate={() => setActiveId(item.id)}
+              />
+            ))}
+          </div>
+          
+          <Outlet/>
+       
+    </div>
+    </>
   )
 }
 
