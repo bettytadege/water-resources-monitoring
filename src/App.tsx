cf0642abe074088e27e82ac5d78a42f832eb3581
bettytadege <bettytadege13@gmail.com>
2025-11-22 11:03:38 +0300

    feat:add new  routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import Features from "./components/feature/Features"
 import DataInegration from "./components/data-integration/DataInegration"
 import Monitoring from "./pages/Monitoring"
 import GeoSpatial from "./pages/GeoSpatial"
-import AiForecasting from "./pages/AiForecasting"
 import EarlyWarning from "./pages/EarlyWarning"
 import SystemTesting from "./components/system-testing/SystemTesting"
 import WaterLevelDashboard from "./components/monitoring/overview/WaterLevelDashboard"
 import SensorNetwork from "./components/monitoring/sensor-network/SensorNetwork"
+import AiForecasting from "./components/monitoring/ai-forecasting/AiForecasting"
+import AiForecastingDashboard from "./pages/AiForecastingDashboard"
+import SatelliteData from "./components/monitoring/satellite-data/SatelliteData"
+import WatershedVisualization from "./components/Geospatial/watersheds/WatershedVisualization"
 
 
 
@@ -16,17 +19,26 @@ function App() {
   return (
     <div className=" w-full h-full ">
       <Routes>
+        {/* home */}
         <Route path="/" element={ <Home/>}>
         <Route path="/" element={ <Features/> }/>
         <Route path="/data-integration" element={ <DataInegration/> }/>
         <Route path="/system-testing" element={ <SystemTesting/> }/>
         </Route>
+        {/* monitoring route */}
         <Route path="/monitoring" element={ <Monitoring/> }>
         <Route path="" element={ <WaterLevelDashboard/> }/>
         <Route path="sensor-network" element={ <SensorNetwork/> }/>
+        <Route path="ai-forecasting" element={ <AiForecasting/> }/>
+        <Route path="satellite-data" element={ <SatelliteData/> }/>
         </Route>
-        <Route path="/geospatial" element={ <GeoSpatial/> }/>
-        <Route path="/ai-forecasting" element={ <AiForecasting/> }/>
+        {/* geospatial route */}
+        <Route path="/geospatial" element={ <GeoSpatial/> }>
+        <Route path="" element={ <WatershedVisualization/> }/>
+        </Route>
+        {/* ai forecasting route */}
+        <Route path="/ai-forecasting" element={ <AiForecastingDashboard/> }/>
+        {/* early warning route */}
         <Route path="/early-warning" element={ <EarlyWarning/> }/>
       </Routes>
      
